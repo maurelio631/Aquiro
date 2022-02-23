05fbb463a9f3a7bc5bf00b32dbd9edf136be2abc
maurelio631 <marco.silva631@outlook.com>
2022-02-23 17:33:46 -0300

    Menu mobile adicionado e  ajustes no tamanho da fonte do banner e do rodapé ajustados

--- script/script.js
+++ script/script.js
@@ -1,9 +1,13 @@
+const toggleButton = document.getElementsByClassName("toggleButton")[0];
+const navbarLinks = document.getElementsByClassName("navbarLinks")[0];
 const divCards = document.getElementById("cards")
 const cards = document.querySelector(".cards").children;
 const listCards = [...cards];
 const detalhes = document.querySelector(".detalhes")
 
-
+toggleButton.addEventListener("click",()=>{
+    navbarLinks.classList.toggle('active')
+})
 
 
 
