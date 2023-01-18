9ec887be6f6b9c60bddf3e549609dfc8b36d435e
Marco <marco.silva631@outlook.com>
2023-01-17 21:45:17 -0300

    Alterações no layout e remoção de código js desnecessário

--- script/script.js
+++ script/script.js
@@ -1,33 +1,16 @@
 const toggleButton = document.getElementsByClassName("toggleButton")[0];
 const navbarLinks = document.getElementsByClassName("navbarLinks")[0];
-const divCards = document.getElementById("cards")
+const divCards = document.getElementById("cards");
 const cards = document.querySelector(".cards").children;
 const listCards = [...cards];
-const detalhes = document.querySelector(".detalhes")
+const detalhes = document.querySelector(".detalhes");
 
 toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle('active')
-})
+  navbarLinks.classList.toggle("active");
+});
 
-
-
-function expandirDetalhes() {
-    listCards.forEach(card => {
-        card.addEventListener("click", () => {
-            if (detalhes) {
-                detalhes.classList.toggle("hidden")
-                console.log(card)
-            }
-        })
-
-    })
-}
-
-expandirDetalhes()
-
-
-const galeria = document.querySelector('.main-gallery')
-var flickty = new Flickity(galeria,{
-    contain: true,
-    autoPlay: true
-})
+const galeria = document.querySelector(".main-gallery");
+var flickty = new Flickity(galeria, {
+  contain: true,
+  autoPlay: true,
+});
