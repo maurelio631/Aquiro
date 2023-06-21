af6169f1ad894f14c5b028e6c269fa1e5c7ec627
maurelio631 <marco.silva631@outlook.com>
2023-06-21 10:47:36 -0300

    Remoção do antigo colaborador daniel do site, inclusão das informações academicas e ajustes na diagramação

--- script/script.js
+++ script/script.js
@@ -9,23 +9,6 @@ toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle('active')
 })
 
-
-
-function expandirDetalhes() {
-    listCards.forEach(card => {
-        card.addEventListener("click", () => {
-            if (detalhes) {
-                detalhes.classList.toggle("hidden")
-                console.log(card)
-            }
-        })
-
-    })
-}
-
-expandirDetalhes()
-
-
 const galeria = document.querySelector('.main-gallery')
 var flickty = new Flickity(galeria,{
     contain: true,
